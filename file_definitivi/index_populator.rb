840761cb199ed4949cc440712555ee7a55e92d23
bolod <federico.spini@email.it>
2009-10-07 13:53:07 +0200

    inserito il file di test e lievemente modificato index_populator

--- file_definitivi/index_populator.rb
+++ file_definitivi/index_populator.rb
@@ -7,11 +7,13 @@ include Ferret::Search
 =begin
 UNO SCHEMA EXCHANGE RAPPRESENTA UN DOCUMENTO
 CAMPI DELL'INDICE
-1) :n_rel	=> numero relazioni
+1) :n_rel		=> numero relazioni
 2) :n_tot_key	=> numero delle chiavi
 3) :n_tot_fkey	=> numero delle chiavi esterne
 4) :cod_rels 	=> #key#fkey#inarcs<codifica  unaria del nemuro degli attributi mediante il carattere 'a'>
 5) :cod_fkey 	=>  rel_<lista ordinata degli identificatori separata da '_' delle relazioni a cui fanno riferimento le chiavi esterne di rel>
+6) :target 	=> xml del target
+7) :file_name	=> file_name
 =end
 
 
@@ -24,8 +26,8 @@ class IndexPopulator
 	def initialize(xml_schema_dir_absolute_path, index_dir)
 			@xml_schema_dir_absolute_path = xml_schema_dir_absolute_path
 			@xtose = XmlToSchemaexchange.new
-			@xtose.parse_dir(dir_absolute_path)
-			@index_name = index_name
+			@xtose.parse_dir(xml_schema_dir_absolute_path)
+			@index_name = index_dir
 			@index = Index.new(:path => index_dir, :create => true)
 			populate_index
 	end
@@ -34,8 +36,8 @@ private
 	def populate_index
 		@xtose.each_schemaexchange { |se| 
 			document = Ferret::Document.new()
-      document[:n_rel]=Ferret::Field.new se.source.length
-      document[:n_tot_key]=Ferret::Field.new se.totkey
+      		document[:n_rel]=Ferret::Field.new se.source.length
+      		document[:n_tot_key]=Ferret::Field.new se.totkey
 			document[:n_tot_fkey]=Ferret::Field.new se.totfkey
 			apprelscod = ""
 			se.source.each { |rel| apprelscod += "#{rel.identify} "}
@@ -43,7 +45,9 @@ private
 			se.each_ref_cod { |cod| apprefcod += "#{cod} "}
 			document[:cod_rels]=Ferret::Field.new apprelscod[0, apprelscod.length-1]
 			#le codifiche delle chiavi esterne vanno separate da spazio?? per ora si
-			document[:cod_fkey  ]=Ferret::Field.new apprefcod[0, apprelscod.length-1]
+			document[:cod_fkey]=Ferret::Field.new apprefcod[0, apprelscod.length-1]
+			document[:target]=Ferret::Field.new se.target
+			document[:xml_file_name]=Ferret::Field.new se.xml_file_name
 			@index << document
 		}
 		@index.optimize()
